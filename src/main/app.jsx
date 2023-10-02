1ac0a87a4d23c37c580639f1208eddf846e16215
FelipeSponge <felipe.fs096@gmail.com>
2023-10-02 20:04:14 -0300

    my-money-app-frontend

---
+++ src/main/app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../common/template/dependencies";
+
+export default function app(props) {
+  return (
+    <div className="wrapper">
+      <h1>App</h1>
+    </div>
+  );
+}
